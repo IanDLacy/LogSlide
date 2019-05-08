01d07fad3520a82f5cb2601e5ea831a3356721f5
Chris Sayen <chrissayen@iMac.local>
2019-05-07 22:12:03 -0400

    this seems needlessly complicated...

--- mySite/p5/sketch.js
+++ mySite/p5/sketch.js
@@ -2,12 +2,15 @@ let font,
   fontsize = 60;
 phrase = 0;
 i = 0;
+cursor = true;
+cursorwait = 10;
+waited = 0;
 gap = 30;
 charr = []
 let phrases = [
     'hey there, buddy...',
-    "hows's things???",
-    "wow. pippy really is a great pup!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    "hows's things?",
+    "wow. pippy really is a great pup!"
 ]
 
 function setup() {
@@ -25,17 +28,31 @@ function draw() {
     for(char = 0; char <= i; char++) {
         text(phrases[phrase].charAt(char), 20 + gap * char, 40);
     }
-    rect(10 + gap * char, 10, 5, 50)
-    fill(0,0,0)
-    i++;
-    if (i == phrases[phrase].length) {
+    if (i != phrases[phrase].length) {
+        i++;
+        rect(10 + gap * char, 10, 5, 50)
+        fill(0,0,0)
+    }else{
+        if (cursor){
+            rect(10 + gap * char, 10, 5, 50)
+            fill(0,0,0)
+            waited++
+            cursor = false;
+        }else{
+            cursor = true;
+        }
+    }
+    
+    if (waited > cursorwait){
         phrase++;
         i = 0;
+        if (phrase == phrases.length) {
+            phrase = 0;
+            i = 0;
+        }
+        waited = 0;
     }
-    if (phrase == phrases.length) {
-        phrase = 0;
-         i = 0;
-    }
+    
 }
 
 
